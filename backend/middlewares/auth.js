706009113307ba25c80f7c3697ee512ff7d42820
Hlengiwe <hlengiwemeld24@gmail.com>
2024-03-24 12:33:47 +0200

    authorized user roles and permissions

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -17,5 +17,14 @@ exports.isAuthenticatedUser = catchAsyncErrors( async(req, res, next) => {
     req.user = await User.findById(decoded.id);
 
     next();
-
 })
+//HANDLING USER ROLES
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(
+                new ErrorHandler(`Role (${req.user.role}) is not allowed to access resource`, 403))
+        }
+        next();
+    }
+}
--- backend/routes/cert.js
+++ backend/routes/cert.js
@@ -12,7 +12,7 @@ const {
     deleteCert 
 } = require('../controllers/certController')
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 //CREATE ROUTE FOR ALL CERTS, GETCERTS FUNCTION HANDLES ROUTE
 router.route('/certifications').get(getCerts); //EXECUTED WHEN GET REQUEST IS MADE
@@ -20,11 +20,12 @@ router.route('/certifications').get(getCerts); //EXECUTED WHEN GET REQUEST IS MA
 //CREATE ROUTE FOR SINGLE CERT, GETSINGLECERT FUNCTION HANDLES ROUTE
 router.route('/certification/:id').get(getSingleCert); 
 
+//----------------------------------------ADMIN ROUTES-----------------------------------------------
 //CREATE ROUTE FOR NEW CERT AND POST TO DATABASE
-router.route('/admin/certification/new').post(isAuthenticatedUser, newCertification);
+router.route('/admin/certification/new').post(isAuthenticatedUser, authorizeRoles('admin'), newCertification);
 
 //CREATE ROUTE FOR UPDATING AND DELETING CERT
-router.route('/admin/certification/:id').put(isAuthenticatedUser, updateCert).delete(isAuthenticatedUser, deleteCert);
+router.route('/admin/certification/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateCert).delete(isAuthenticatedUser, authorizeRoles('admin'), deleteCert);
 
 
 //EXPORT ROUTER OBJECT FOR OTHER PARTS OF APP TO USE IT
