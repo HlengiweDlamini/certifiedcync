8085822811aa9d37e026f15fbb5ab34beccec3e0
Hlengiwe <hlengiwemeld24@gmail.com>
2024-03-24 19:28:57 +0200

    generated forgot password token

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -52,6 +53,22 @@ userSchema.methods.getJwtToken = function () {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_TIME
     });
+}
+
+//GENERATE PASSWORD REST TOKEN
+userSchema.methods.getResetPasswordToken = function() {
+    //GENERATE TOKEN
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    //HASH AND SET TO RESETPASSWORDTOKEN
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    //SET TOKEN EXPIRY TIME
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
+
+
 }
 
 //EXPORT
