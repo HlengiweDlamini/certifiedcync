bf013873dfb76621f69ae09143c3d219a511cdcd
Hlengiwe <hlengiwemeld24@gmail.com>
2024-03-24 22:04:12 +0200

    handling wrong and expired jwt token

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -25,12 +25,29 @@ module.exports = (err, req, res, next) => {
             const message = `Resource not found. Invalid: ${err.path}`
             error = new ErrorHandler(message, 400)
         }
-        //HANDLING MONGOOSE VALIDATION ERROR
+        //HANDLE MONGOOSE VALIDATION ERROR
         if (err.name === 'ValidationError') {
             const message = Object.values(err.errors).map(value =>  value.message);
             error = new ErrorHandler(message, 400)
         }
 
+        //HANDLE MONGOOSE DUPLICATE KEY ERROR
+        if (err.code === 11000) {
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+            error = new ErrorHandler(message, 400);
+        }
+
+        //HANDLE WRONG JWT ERROR
+        if (err.name === 'JsonWebTokenError') {
+            const message = 'JSON Web Token is invalid. Try again!!!'
+            error = new ErrorHandler(message, 400)
+        }
+
+        //HANDLE EXPIRED JWT ERROR
+        if (err.name === 'TokenExpiredError') {
+            const message = 'JSON Web Token has expired. Try again!!!'
+            error = new ErrorHandler(message, 400)
+        }
 
         res.status(error.statusCode).json({
             success: false,
