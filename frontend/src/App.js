11c7fdbe8386eb9475a8bae1161a001c2cab5530
Hlengiwe <hlengiwemeld24@gmail.com>
2024-03-26 03:11:34 +0200

    install react and dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
